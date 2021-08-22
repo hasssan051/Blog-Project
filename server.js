2024f9503140cefacaafd0e5c08882d2ba7e5aa4
iamzaidi <hassanzaidi4@gmail.com>
2021-08-23 01:46:55 +0500

    Completed the basics of editor. html
    The basic version of editor.html or the editor page of the blog has been completed with the server now also containing routes for uploaded images for a new blog post.

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const path = require('path');
 const PORT ="3000"
+const fileUpload = require('express-fileupload');
 
 let initial_path = path.join(__dirname, "public");
 
 const app = express();
 app.use(express.static(initial_path));
+app.use(fileUpload())
 
 
 app.get('/', (req, res) => {
@@ -15,6 +17,25 @@ app.get('/', (req, res) => {
 
 app.get('/editor', (req, res)=>{
     res.sendFile(path.join(initial_path,'editor.html'));
+})
+
+//upload link
+app.post('/upload',(req,res)=> {
+    let file= req.file.image;
+    let date= new Date();
+    //image name
+    let imageName = date.getDate() +date.getTime() + file.name;
+    //path upload for image
+    let path='public/uploads/'+imageName;
+    //create the upload 
+    file.mv(path, (err, result)=> {
+        if(err){
+            throw err;
+        }else {
+            //image's upload path 
+            res.json(`uploads/${imagename}`);
+        }
+    })
 })
 
 app.listen("3000", () => {
