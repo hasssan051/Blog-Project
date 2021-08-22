188103d93b03f212b7880a4fdbcaff4c03142c05
iamzaidi <hassanzaidi4@gmail.com>
2021-08-23 01:32:16 +0500

    Made initial template for home page of website
    Used to basic html and css to make the home page of the blog. Also have the server.js running.

---
+++ server.js
@@ -0,0 +1,22 @@
+
+const express = require('express');
+const path = require('path');
+const PORT ="3000"
+
+let initial_path = path.join(__dirname, "public");
+
+const app = express();
+app.use(express.static(initial_path));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(initial_path, "home.html"));
+});
+
+app.get('/editor', (req, res)=>{
+    res.sendFile(path.join(initial_path,'editor.html'));
+})
+
+app.listen("3000", () => {
+    console.log("listening.....");
+});
